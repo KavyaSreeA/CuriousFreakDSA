68aba51c90a9a30809a9954f226f35854686d0b6
Kavya <kavyaarunbalaji@gmail.com>
2025-08-19 19:57:26 +0530

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,46 @@
+Problem 1:
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class pattern
+{
+	public static void main (String[] args) 
+	{
+		int N= 4;
+		for(int i = 1 ; i <= N ; i++){
+		    for(int j = 1 ; j <= N - i + 1 ; j++){
+		        System.out.print("*");
+		    }
+		    System.out.println(" ");
+		}
+		
+
+	}
+}
+
+
+Problem 2:
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class pattern
+{
+	public static void main (String[] args) 
+	{
+		int N= 6;
+		for(int i = 1 ; i <= N ; i++){
+		    for(int j = 1 ; j <= N - i + 1 ; j++){
+		        System.out.print("*");
+		    }
+		    System.out.println(" ");
+		}
+		
+
+	}
+}
+
+
