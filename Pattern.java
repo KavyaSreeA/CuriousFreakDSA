4efd26d02131289df5e6877c5c5568048da97664
Kavya <kavyaarunbalaji@gmail.com>
2025-08-19 20:16:05 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -43,4 +43,126 @@ public class pattern
 	}
 }
 
+Problem 3:
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class pattern
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int N= 6;
+		for(int i = 1 ; i <= N ; i++){
+		    for(int j = N - i + 1 ; j >= 1 ; j--){
+		        System.out.print(j );
+		    }
+		    System.out.println(" ");
+		}
+		
+
+	}
+}
+
+output:
+
+654321 
+54321 
+4321 
+321 
+21 
+1  
+
+problem 4:
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class pattern 
+{
+	public static void main (String[] args) 
+	{
+		int N= 4;
+		for(int i = 1 ; i <= N ; i++){
+		    for(int j = N - i + 1 ; j >= 1 ; j--){
+		        System.out.print(j );
+		    }
+		    System.out.println(" ");
+		}
+		
+
+	}
+}
+
+Output :
+4321
+
+321
+
+21
+
+1
+
+Problem 5:
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Pattern
+{
+	public static void main (String[] args) 
+	{
+		int N= 4;
+		for(int i = 1 ; i <= N ; i++){
+		    for(int j = 1; j <=  i  ; j++){
+		        System.out.print("*");
+		    }
+		    System.out.println(" ");
+		}
+		
+
+	}
+}
+
+Output:
+* 
+** 
+*** 
+****  
+
+Problem 6:
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Pattern
+{
+	public static void main (String[] args) 
+	{
+		int N= 6;
+		for(int i = 1 ; i <= N ; i++){
+		    for(int j = 1; j <=  i  ; j++){
+		        System.out.print("*");
+		    }
+		    System.out.println(" ");
+		}
+		
+
+	}
+}
+
+Output:
+
+* 
+** 
+*** 
+**** 
+***** 
+****** 
+
+
+
 
